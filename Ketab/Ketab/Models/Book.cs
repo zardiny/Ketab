ad69e8d0a890d657d9e129a98186e1efdc5196df
Ali Zare <a.zardiny@kandaidea.com>
2022-12-22 10:43:59 +0330

    First Commit

---
+++ Ketab/Ketab/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace Ketab
+{
+    public class Book
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Writer { get; set; }
+        public int Year { get; set; }
+    }
+}
